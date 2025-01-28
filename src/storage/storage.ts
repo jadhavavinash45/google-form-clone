4a91af37c6280e0aabea7ae33ae87b224f347429
Avinash Jadhav <avinanuami@gmail.com>
2025-01-28 19:38:27 +0530

    new file: added storage file to work with local storage

---
+++ src/storage/storage.ts
@@ -0,0 +1,20 @@
+import { Form, FormResponse } from "../types/interfaces";
+
+const FORM_STORAGE_KEY = 'forms';
+const RESPONSE_STORAGE_KEY = 'responses';
+
+export const saveForm = (form: Form): void => {
+  const existingForms = loadForms();
+  const index = existingForms.findIndex(f => f.id === form.id);
+  if (index >= 0) existingForms[index] = form;
+  else existingForms.push(form);
+  localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(existingForms));
+};
+
+export const loadForms = (): Form[] => {
+  return JSON.parse(localStorage.getItem(FORM_STORAGE_KEY) || '[]');
+};
+
+export const saveResponse = (response: FormResponse): void => {
+  // Similar implementation
+};
